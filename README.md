e0c984e5f0ea6577850d29f41c343dc88f510d50
Slys <slysdeveloper@gmail.com>
2022-06-09 17:02:58 -0400

    Added welcome screen, Firebase authentication, and subsequent login/register screens. Starting to build the homescreen UI

---
+++ README.md
@@ -0,0 +1,5 @@
+# habitur
+
+Habitur is a habit building app centered around the idea of "Kaizen", the art of incremental progress.
+
+
---
+++ android/app/src/main/kotlin/com/example/habitur/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.habitur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
