3e40580069c6328fbec9f6ae10ec44565dc0056f
Slys <slysdeveloper@gmail.com>
2024-06-26 22:02:45 +0200

    Add line graph to overview; create static card

---
+++ lib/components/static_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:habitur/constants.dart';
+
+class StaticCard extends StatelessWidget {
+  StaticCard({super.key, required this.child});
+  Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: kFadedBlue.withOpacity(0.5),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                offset: Offset(0, 10),
+                spreadRadius: 1,
+                blurRadius: 30),
+          ]),
+      child: child,
+    );
+  }
+}
--- lib/screens/habit_overview_screen.dart
+++ lib/screens/habit_overview_screen.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:habitur/components/line_graph.dart';
+import 'package:habitur/components/static_card.dart';
+import 'package:habitur/constants.dart';
 import 'package:habitur/models/habit.dart';
+import 'package:habitur/providers/summary_statistics_repository.dart';
+import 'package:provider/provider.dart';
 
 class HabitOverviewScreen extends StatelessWidget {
-  const HabitOverviewScreen({super.key, required Habit habit});
+  Habit habit;
+  HabitOverviewScreen({super.key, required this.habit});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('Overview Screen'),
+      body: SafeArea(
+        child: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Text(
+                  habit.title,
+                  style: kTitleTextStyle.copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                StaticCard(
+                  child: LineGraph(
+                    data: Provider.of<SummaryStatisticsRepository>(context)
+                        .confidenceStats,
+                    height: 300,
+                    yAxisTitle: 'Confidence Level',
+                    showDots: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
