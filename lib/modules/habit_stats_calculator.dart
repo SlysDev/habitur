510b1f5441e47fb3e24b6b9522214acbe1e86fdb
Slys <slysdeveloper@gmail.com>
2024-07-09 20:08:35 +0200

    rework confidenceLevel calculation + adjust completion slope calculation

--- lib/modules/habit_stats_calculator.dart
+++ lib/modules/habit_stats_calculator.dart
@@ -21,6 +21,7 @@ class HabitStatisticsCalculator extends StatisticsCalculator {
     double consistencyFactor =
         calculateConsistencyFactor(habit.stats, habit.requiredCompletions);
     double successStreakBonus = 1.0;
+    double difficultyWeight = 1 - (habit.stats.last.difficultyRating / 10);
 
     if (habit.streak > 0) {
       successStreakBonus =
@@ -29,6 +30,55 @@ class HabitStatisticsCalculator extends StatisticsCalculator {
 
     print(
         'Confidence level: base: $baseConfidence * consistency: $consistencyFactor * streak bonus: $successStreakBonus}');
-    return baseConfidence * consistencyFactor * successStreakBonus;
+    return baseConfidence *
+        consistencyFactor *
+        successStreakBonus *
+        difficultyWeight;
+  }
+
+  @override
+  double calculateStatSlope(String statisticName, List<StatPoint> stats,
+      {int period = 7}) {
+    if (stats.isEmpty || period <= 0) {
+      return 0.0; // Handle empty data or invalid period
+    }
+
+    double slopeCorrection = 0.0; // corrects for high required completions
+    if (statisticName == 'completions') {
+      slopeCorrection = habit.requiredCompletions.toDouble();
+    }
+
+    if (stats.length < period) {
+      period = stats.length; // Limit period to available data
+    }
+
+    double sumX = 0.0;
+    double sumY = 0.0;
+    double sumXY = 0.0;
+    double sumX2 = 0.0;
+
+    for (int i = stats.length - period; i < stats.length; i++) {
+      if (i >= 0) {
+        int dayIndex =
+            i + 1 - (stats.length - period); // Adjust day index based on period
+        double statisticValue =
+            getStatisticValue(stats[i], statisticName).toDouble();
+        sumX += dayIndex;
+        sumY += statisticValue;
+        sumXY += dayIndex * statisticValue;
+        sumX2 += dayIndex * dayIndex;
+      }
+    }
+
+    int n = period; // Number of data points in the specified period
+    double denominator = n * sumX2 - sumX * sumX;
+
+    // Check for colinearity or potential numerical errors
+    if (denominator.abs() < 1e-6) {
+      return 0.0; // Handle colinearity or very small denominator
+    }
+
+    double slope = (n * sumXY - sumX * sumY) / denominator * slopeCorrection;
+    return slope;
   }
 }
