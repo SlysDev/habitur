299a49c4646ddd0ec8ab77c685a95e6a6b37b6bb
Slys <slysdeveloper@gmail.com>
2024-10-14 15:24:35 -0700

    feat: add show error dialog util func

--- lib/util_functions.dart
+++ lib/util_functions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habitur/components/error_tile.dart';
 import 'package:habitur/constants.dart';
 
 // error handling
@@ -11,6 +12,33 @@ showDebugErrorSnackbar(BuildContext context, e, s) {
           'Error--Take a screenshot and send to our team: \n \n ${e.toString()}, $s'),
     ),
   );
+}
+
+showErrorDialog(BuildContext context, String errorText) {
+  showDialog(
+    barrierColor: Colors.transparent,
+    barrierDismissible: true,
+    context: context,
+    builder: (context) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          AlertDialog(
+            alignment: Alignment.bottomCenter,
+            backgroundColor: kRed.withOpacity(0.8),
+            content: ErrorTile(
+              errorText: errorText,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 60),
+        ],
+      );
+    },
+  );
+  Future.delayed(Duration(seconds: 2), () {
+    Navigator.pop(context);
+  });
 }
 
 // UI transitions
