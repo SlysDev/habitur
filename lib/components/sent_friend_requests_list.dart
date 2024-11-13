de61b150c14c46bad6954ae456fca406324e9dc1
Slys <slysdeveloper@gmail.com>
2024-11-12 16:10:55 -0800

    feat: add green/red hues + less opacity for accepted/not accepted friend requests

--- lib/components/sent_friend_requests_list.dart
+++ lib/components/sent_friend_requests_list.dart
@@ -5,6 +5,7 @@ import 'package:habitur/models/friend_request.dart';
 import 'package:habitur/data/remote/user_database.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
+import '../constants.dart';
 import '../models/user.dart';
 
 class SentFriendRequestsList extends StatelessWidget {
@@ -17,42 +18,62 @@ class SentFriendRequestsList extends StatelessWidget {
           return CircularProgressIndicator();
         }
         if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Container(
-            padding: EdgeInsets.all(16.0),
-            child: Text('No sent friend requests.'),
-            );
+          return Text('No sent friend requests.');
         }
-        return Container(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: snapshot.data!.length,
-            padding: EdgeInsets.zero,
-            itemBuilder: (context, index) {
-              FriendRequest request = snapshot.data![index];
-              return FutureBuilder<UserModel?>(
-                future: UserDatabase().getUserModelById(request.recipientUid),
-                builder: (context, userSnapshot) {
-                  if (userSnapshot.connectionState == ConnectionState.waiting) {
-                    return ListTile(
-                      title: Text('Loading...'),
-                    );
-                  }
-                  if (!userSnapshot.hasData) {
-                    return ListTile(
-                      title: Text('User not found'),
-                    );
-                  }
-                  UserModel recipient = userSnapshot.data!;
-                  String relativeDate = timeago.format(request.dateSent);
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            FriendRequest request = snapshot.data![index];
+            return FutureBuilder<UserModel?>(
+              future: UserDatabase().getUserModelById(request.recipientUid),
+              builder: (context, userSnapshot) {
+                if (userSnapshot.connectionState == ConnectionState.waiting) {
                   return ListTile(
-                    title: Text(recipient.username),
-                    subtitle: Text('Sent $relativeDate'),
-                    trailing: Text(request.isAccepted ? 'Accepted' : 'Pending'),
+                    title: Text('Loading...'),
                   );
-                },
-              );
-            },
-          ),
+                }
+                if (!userSnapshot.hasData) {
+                  return ListTile(
+                    title: Text('User not found'),
+                  );
+                }
+                UserModel recipient = userSnapshot.data!;
+                String relativeDate = timeago.format(request.dateSent);
+                return Opacity(
+                  opacity: request.isAccepted || request.isDeclined ? 0.5 : 1,
+                  child: Container(
+                    color: request.isAccepted
+                        ? kFadedGreen.withOpacity(0.4)
+                        : request.isDeclined
+                            ? kFadedRed.withOpacity(0.4)
+                            : Colors.transparent,
+                    child: ListTile(
+                      title: Text(
+                        recipient.username,
+                      ),
+                      subtitle: Text('Sent $relativeDate'),
+                      trailing: Text(
+                        request.isAccepted
+                            ? 'Accepted'
+                            : request.isDeclined
+                                ? 'Declined'
+                                : 'Pending',
+                        style: TextStyle(
+                          color: request.isAccepted
+                              ? kLightGreenAccent
+                              : request.isDeclined
+                                  ? kLightRedAccent
+                                  : null,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         );
       },
     );
